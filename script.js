d0dca4270a7e07d49673cb631608dae8670c0c4a
Jay Lim <jaylim106@gmail.com>
2017-06-28 11:25:13 -0700

    trying to figure out encapsulation problem

--- script.js
+++ script.js
@@ -2,56 +2,47 @@
 // var popular_movies = [];
 // popular movies list
 var api_key = "1c7597f95f188897693c3ccde9dc7a66";
-$(document).ready(function(){
-    $.ajax({
-        url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + api_key + '&language=en-US&page=1',
-        dataType: 'json',
-        jsonpCallback: 'testing',
-        success: function(result){
-            console.log("success: ", result);
-            var popular_movies = result.results;
-            displayMovies(popular_movies);
-        },
-        error: function(result){
-         console.log("error");
-        }
-    });
-});
 
 
 
 function displayMovies(popular_movies){
-    console.log(popular_movies.length);
+    console.log("poop: ",popular_movies);
     for(var i=0; i < popular_movies.length; i++){
 
         var image = $("<img>").attr("src", "https://image.tmdb.org/t/p/original" + popular_movies[i].poster_path);
         var img_link = $("<a>", {href: "review.html"}).append(image);
         var img_container = $("<div>").append(img_link).appendTo(".container");
         img_container.click(function(){
-            //have access to popular_movies[i]
+            //have access to popular_movies[i]\
+            doStuff(i, popular_movies);
+            console.log("i: ", i);
+            console.log("popular movies: ", popular_movies);
             movieInfo(popular_movies[i]);
+            reddit(popular_movies[i]);
         });
     }
 };
+
+function doStuff
 
 function movieInfo(movieObj){
     //  var api_key = "1c7597f95f188897693c3ccde9dc7a66";
 //     $(document).ready(function(){
     console.log("inside movieinfo function");
-      $.ajax({
-          url: 'https://api.themoviedb.org/3/movie/297762?api_key=' + api_key + '&language=en-US',
-          method: "get",
-          async: true,
-          headers: {},
-          data: {},
-          crossDomain: true,
-          success: function(result){
-              console.log("success: ", result);
-          },
-          error: function(result){
-              console.log("error: ", result);
-          }
-      });
+      // $.ajax({
+      //     url: 'https://api.themoviedb.org/3/movie/297762?api_key=' + api_key + '&language=en-US',
+      //     method: "get",
+      //     async: true,
+      //     headers: {},
+      //     data: {},
+      //     crossDomain: true,
+      //     success: function(result){
+      //         console.log("success: ", result);
+      //     },
+      //     error: function(result){
+      //         console.log("error: ", result);
+      //     }
+      // });
 //     });
 }
 
