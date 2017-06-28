f619ccc6855f740472d5ace8e690f16a57e33aa3
Jay Lim <jaylim106@gmail.com>
2017-06-28 10:56:24 -0700

    resolve conflicts

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-var popular_movies = [];
+// var popular_movies = [];
 // popular movies list
 var api_key = "1c7597f95f188897693c3ccde9dc7a66";
 $(document).ready(function(){
     $.ajax({
-        url: 'https://api.themoviedb.org/3/movie/popular?api_key=' + api_key + '&language=en-US&page=1',
+        url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + api_key + '&language=en-US&page=1',
         dataType: 'json',
         jsonpCallback: 'testing',
         success: function(result){
             console.log("success: ", result);
-            popular_movies = result.results;
-            displayMovies();
+            var popular_movies = result.results;
+            displayMovies(popular_movies);
         },
         error: function(result){
          console.log("error");
@@ -20,16 +20,47 @@ $(document).ready(function(){
 
 
 
-function displayMovies(){
+function displayMovies(popular_movies){
     console.log(popular_movies.length);
     for(var i=0; i < popular_movies.length; i++){
 
         var image = $("<img>").attr("src", "https://image.tmdb.org/t/p/original" + popular_movies[i].poster_path);
-        // var img_link =
-        var img_container = $("<div>").append(image).appendTo(".container");
+        var img_link = $("<a>", {href: "review.html"}).append(image);
+        var img_container = $("<div>").append(img_link).appendTo(".container");
         img_container.click(function(){
-
-
+            //have access to popular_movies[i]
+            movieInfo(popular_movies[i]);
         });
     }
+};
+
+function movieInfo(movieObj){
+    //  var api_key = "1c7597f95f188897693c3ccde9dc7a66";
+//     $(document).ready(function(){
+    console.log("inside movieinfo function");
+      $.ajax({
+          url: 'https://api.themoviedb.org/3/movie/297762?api_key=' + api_key + '&language=en-US',
+          method: "get",
+          async: true,
+          headers: {},
+          data: {},
+          crossDomain: true,
+          success: function(result){
+              console.log("success: ", result);
+          },
+          error: function(result){
+              console.log("error: ", result);
+          }
+      });
+//     });
 }
+
+
+
+// "async": true,
+//     "crossDomain": true,
+//     "url": "https://api.themoviedb.org/3/movie/297762?language=en-US&api_key=1c7597f95f188897693c3ccde9dc7a66",
+//     "method": "GET",
+//     "headers": {},
+// "data": "{}"
+// // Object {readyState: 0, getResponseHeader: function, getAllResponseHeaders: function, setRequestHeader: function, overrideMimeType: function…}
